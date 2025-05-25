9be7da8c315e5d965e5debf5b5e2510fb2d1995e
Dev-Teaplus <teaplus.suwan@gmail.com>
2025-05-25 17:43:35 +0700

    โครงเสร็จ

--- InfoBridge/src/app/app.routes.ts
+++ InfoBridge/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './features/homepage/homepage.component';
+import { DocumentListComponent } from './features/document-list/document-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'documents', component: DocumentListComponent },
+];
---
+++ InfoBridge/src/app/features/document-list/document-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-document-list',
+  imports: [],
+  templateUrl: './document-list.component.html',
+  styleUrls: ['./document-list.component.scss']
+})
+export class DocumentListComponent {
+
+
+}
