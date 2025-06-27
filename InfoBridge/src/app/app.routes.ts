e595752a2a21fc549735049a6f8e35eb7ac9dea5
Dev-Teaplus <teaplus.suwan@gmail.com>
2025-06-27 15:45:31 +0700

    เปลื่ยนไลบาร์รี่

--- InfoBridge/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomepageComponent } from './features/homepage/homepage.component';
-import { DocumentListComponent } from './features/document-list/document-list.component';
-
-export const routes: Routes = [
-  { path: '', component: HomepageComponent },
-  { path: 'documents', component: DocumentListComponent },
-];
